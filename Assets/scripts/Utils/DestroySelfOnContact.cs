dbf01a8054e729f207b32db719958517515b5331
Fabian Mauz <fmauz@ipb-halle.de>
2023-09-13 10:15:26 +0200

    add projectiles

---
+++ Assets/scripts/Utils/DestroySelfOnContact.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelfOnContact : MonoBehaviour{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/scripts/Utils/Lifetime.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour{
+    // Start is called before the first frame update
+    [SerializeField] 
+    private float lifeTimeInSeconds=1;
+    void Start(){
+        GameObject.Destroy(this.gameObject,lifeTimeInSeconds);
+        
+    }
+
+    
+    
+}
